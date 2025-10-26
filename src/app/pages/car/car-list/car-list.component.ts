5245e3ecca4944dbb3e2e99430c40cafd7d94d76
Rennan Marcile Lazarini <rennanml112@gmail.com>
2025-10-26 01:03:59 -0300

    wip: car pages impl

--- src/app/pages/car/car-list/car-list.component.ts
+++ src/app/pages/car/car-list/car-list.component.ts
@@ -1,11 +1,67 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { CarService } from '../../../services/car.service';
+import { Car } from '../../../model/car.model';
+
+import { CommonModule } from '@angular/common';
+import { MatTableModule } from '@angular/material/table';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @Component({
   selector: 'app-car-list',
-  imports: [],
+  standalone: true,
+  imports: [
+    CommonModule,
+    MatTableModule,
+    MatButtonModule,
+    MatIconModule,
+    MatCardModule,
+    MatTooltipModule
+  ],
   templateUrl: './car-list.component.html',
-  styleUrl: './car-list.component.css'
+  styleUrls: ['./car-list.component.css']
 })
-export class CarListComponent {
+export class CarListComponent implements OnInit {
+  cars: Car[] = [];
+  
+  displayedColumns: string[] = ['licensePlate', 'brand', 'model', 'basePrice', 'actions'];
 
+  private carService = inject(CarService);
+  private router = inject(Router);
+  private snackBar = inject(MatSnackBar);
+
+  ngOnInit(): void {
+    this.loadCars();
+  }
+
+  loadCars(): void {
+    this.carService.getCars().subscribe({
+      next: (data) => this.cars = data,
+      error: (err) => this.snackBar.open('Erro ao carregar carros.', 'Fechar', { duration: 3000 })
+    });
+  }
+
+  editCar(licensePlate: string): void {
+    this.router.navigate(['/cars/edit', licensePlate]);
+  }
+
+  deleteCar(licensePlate: string): void {
+    if (confirm('Tem certeza que deseja excluir este carro?')) {
+      this.carService.deleteCar(licensePlate).subscribe({
+        next: () => {
+          this.snackBar.open('Carro excluído com sucesso!', 'Fechar', { duration: 3000 });
+          this.loadCars();
+        },
+        error: (err) => this.snackBar.open('Erro ao excluir carro.', 'Fechar', { duration: 3000 })
+      });
+    }
+  }
+
+  addCar(): void {
+    this.router.navigate(['/cars/new']);
+  }
 }
--- src/app/services/car.service.ts
+++ src/app/services/car.service.ts
@@ -1,9 +1,33 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Car } from '../model/car.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CarService {
+  private readonly API_URL = 'http://localhost:8080/api/v1/cars';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getCars(): Observable<Car[]> {
+    return this.http.get<Car[]>(this.API_URL);
+  }
+
+  getCarByLicensePlate(licensePlate: string): Observable<Car> {
+    return this.http.get<Car>(`${this.API_URL}/${licensePlate}`);
+  }
+
+  createCar(car: Car): Observable<Car> {
+    return this.http.post<Car>(this.API_URL, car);
+  }
+
+  updateCar(licensePlate: string, car: Car): Observable<Car> {
+    return this.http.put<Car>(`${this.API_URL}/${licensePlate}`, car);
+  }
+
+  deleteCar(licensePlate: string): Observable<void> {
+    return this.http.delete<void>(`${this.API_URL}/${licensePlate}`);
+  }
 }

3ffa37fbc07beb8565aa83b179bffca4f8500e6b
Abel Baes <abel.baes77@gmail.com>
2025-10-26 19:29:59 -0300

    feat(rental): adding rental list page

--- src/app/services/rental.service.ts
+++ src/app/services/rental.service.ts
@@ -1,9 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Rental } from '../model/rental.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RentalService {
+  private readonly API_URL = 'http://localhost:8080/api/v1/rentals';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getRentals(): Observable<Rental[]> {
+    return this.http.get<Rental[]>(this.API_URL);
+  }
 }

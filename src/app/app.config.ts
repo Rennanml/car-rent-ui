ef14cf418ded06200f41ba332ecf1fad8661d2de
Abel Baes <abel.baes77@gmail.com>
2025-11-03 21:04:36 -0300

    feat(app.configs): adding error interceptor in app config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,12 +4,17 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { authInterceptor } from './services/auth/auth.interceptor';
+import { errorInterceptor } from './interceptors/error.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideHttpClient(
-      withInterceptors([authInterceptor])
-    )]
+      withInterceptors([
+        authInterceptor,
+        errorInterceptor
+      ])
+    )
+  ]
 };
